812fd6ba6c14698a4eda799bc8706fc35889db74
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-06-26 22:44:46 +0800

    new version

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="pybind11_union_find",
-    version="0.0.1",
+    version="0.0.2",
     author="tzx",
     author_email="dvorak4tzx@gmail.com",
     url="https://pybind11-union-find.readthedocs.io",
